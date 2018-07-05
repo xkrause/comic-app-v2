fc3d8050f3b07bce02393df57f1b0bffb910c697
Xander Krause <sk8rak@gmail.com>
2018-07-05 16:23:22 -0700

    Connecting to the database

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Connecting to the database
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/comicsCollection');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
